e3563bff0b9b593b14963a3797b1b45496f392dd
Ramzay <ramzisiala@gmail.com>
2020-07-16 17:05:07 +0200

    Log4j (#8)
    * Log4j 2 implementation
    
    - Include log4j2 maven dependency and configuration
    - Allow users to enable full log
    - Code cleaning and improvements

--- src/main/java/com/smart/home/pc/daemon/impl/Daemon.java
+++ src/main/java/com/smart/home/pc/daemon/impl/Daemon.java
@@ -11,116 +11,109 @@ import com.smart.home.pc.daemon.service.impl.SimpleCustomTasksImpl;
 
 public class Daemon {
 
-	private static final Date PROC_START_DATE = new Date();
+    private static final Date PROC_START_DATE = new Date();
 
-	private String commandDirAbsPath;
+    private SimpleCustomTasks simpleCustomTasks;
 
-	private SimpleCustomTasks simpleCustomTasks;
+    public Daemon() {
+        simpleCustomTasks = new SimpleCustomTasksImpl();
+    }
 
-	private FullConfiguration configuration;
+    public void run(FullConfiguration configuration) throws RuntimeException, IOException {
+        // Initialise
+        initProc(configuration);
 
-	public Daemon(FullConfiguration configuration) {
-		commandDirAbsPath = configuration.getUserConfiguration().getCommandPath();
-		simpleCustomTasks = new SimpleCustomTasksImpl();
-		this.configuration = configuration;
-	}
+        // Run proc
+        Command command = getCommand(configuration);
+        if (command != null) {
+            // Delete files associated to this command
+            deleteAllFiles(command.getFileName(), configuration);
+            // Execute shutdown action
+            simpleCustomTasks.executeCommand(configuration, command);
+        }
 
-	public void run() throws RuntimeException, IOException {
-		// Initialise
-		initProc();
+        // Final proc
+    }
 
-		// Run proc
-		Command command = getCommand();
-		if (command != null) {
-				// Delete files associated to this command
-				deleteAllFiles(command.getFileName());
-				// Execute shutdown action
-				simpleCustomTasks.executeCommand(configuration, command);
-			
-		}
+    /**
+     * Initialisation for the process: - Delete all files older than the process
+     * start date time
+     */
+    private void initProc(FullConfiguration configuration) {
+        // Delete all files that are older than the start procedure date
+        File[] filesInCommandFolder = getFilesInCommandDirLocation(configuration);
+        if (filesInCommandFolder != null) {
+            for (int i = 0; i < filesInCommandFolder.length; i++) {
+                File currentFile = filesInCommandFolder[i];
+                if (currentFile != null && currentFile.isFile()) {
+                    Date fileLastModified = new Date(currentFile.lastModified());
+                    if (PROC_START_DATE.after(fileLastModified)) {
+                        currentFile.delete();
+                    }
+                }
+            }
+        }
+    }
 
-		// Final proc
-	}
+    /**
+     * Function that will check if there are any pending actions to perform based on
+     * file name located in special directory if specific file is detected we return
+     * the underlying action to perform
+     * 
+     * @return Command : command to be performed
+     */
+    private Command getCommand(FullConfiguration configuration) {
 
-	/**
-	 * Initialisation for the process: - Delete all files older than the process
-	 * start date time
-	 */
-	private void initProc() {
-		// Delete all files that are older than the start proc date
-		File[] filesInCommandFolder = getFilesInCommandDirLocation();
-		if (filesInCommandFolder != null) {
-			for (int i = 0; i < filesInCommandFolder.length; i++) {
-				File currentFile = filesInCommandFolder[i];
-				if (currentFile != null && currentFile.isFile()) {
-					Date fileLastModified = new Date(currentFile.lastModified());
-					if (PROC_START_DATE.after(fileLastModified)) {
-						currentFile.delete();
-					}
-				}
-			}
-		}
-	}
+        // Directory exists we check files inside
+        File[] listOfFiles = getFilesInCommandDirLocation(configuration);
+        if (listOfFiles != null && listOfFiles.length == 0) {
+            return null;
+        }
 
-	/**
-	 * Function that will check if there are any pending actions to perform based on
-	 * file name located in special directory if specific file is detected we return
-	 * the underlying action to perform
-	 * 
-	 * @return Command : command to be performed
-	 */
-	private Command getCommand() {
+        // Check if there is a trigger file in the directory
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile()) { // Make sure it's a file and not a dir
+                for (Command currentCommand : configuration.getUserConfiguration().getCommands()) {
+                    if (currentCommand.getFileName() != null && listOfFiles[i].getName().contains(currentCommand.getFileName())) {
+                        return currentCommand; // TODO: list of commands ?
+                    }
+                }
+            }
+        }
+        return null;
+    }
 
-		// Directory exists we check files inside
-		File[] listOfFiles = getFilesInCommandDirLocation();
-		if (listOfFiles != null && listOfFiles.length == 0) {
-			return null;
-		}
+    /**
+     * Deletes all files that has the given parameter included in their name
+     * 
+     * @param name string that is used to find the files to delete
+     */
+    private void deleteAllFiles(String name, FullConfiguration configuration) {
+        // Delete all files that contains the command in parameter
+        File[] filesInCommandFolder = getFilesInCommandDirLocation(configuration);
+        if (filesInCommandFolder != null) {
+            for (int i = 0; i < filesInCommandFolder.length; i++) {
+                File currentFile = filesInCommandFolder[i];
+                if (currentFile != null && currentFile.isFile()) {
+                    if (currentFile.getName().toLowerCase().contains(name.toLowerCase())) {
+                        currentFile.delete();
+                    }
+                }
+            }
+        }
+    }
 
-		// Check if there is a trigger file in the directory
-		for (int i = 0; i < listOfFiles.length; i++) {
-			if (listOfFiles[i].isFile()) { // Make sure it's a file and not a dir				
-				for(Command currentCommand : configuration.getUserConfiguration().getCommands()) {
-					if (currentCommand.getFileName() != null && listOfFiles[i].getName().contains(currentCommand.getFileName())) {
-						return currentCommand; // TODO: list of commands ?
-					}
-				}				
-			}
-		}
-		return null;
-	}
-
-	/**
-	 * Deletes all files that has the given parameter included in their name
-	 * 
-	 * @param name string that is used to find the files to delete
-	 */
-	private void deleteAllFiles(String name) {
-		// Delete all files that contains the command in parameter
-		File[] filesInCommandFolder = getFilesInCommandDirLocation();
-		if (filesInCommandFolder != null) {
-			for (int i = 0; i < filesInCommandFolder.length; i++) {
-				File currentFile = filesInCommandFolder[i];
-				if (currentFile != null && currentFile.isFile()) {
-					if (currentFile.getName().toLowerCase().contains(name.toLowerCase())) {
-						currentFile.delete();
-					}
-				}
-			}
-		}
-	}
-
-	/**
-	 * Function that will check base folder existence
-	 * 
-	 * @return files in base directory if it exists otherwise null
-	 */
-	private File[] getFilesInCommandDirLocation() {
-		File folder = new File(commandDirAbsPath);
-		if (folder != null && folder.exists() && folder.isDirectory()) {
-			return folder.listFiles();
-		}
-		return new File[0];
-	}
+    /**
+     * Function that will check base folder existence
+     * 
+     * @return files in base directory if it exists otherwise null
+     */
+    private File[] getFilesInCommandDirLocation(FullConfiguration configuration) {
+        File folder = new File(configuration.getUserConfiguration().getCommandPath());
+        if (folder != null && folder.exists() && folder.isDirectory()) {
+            return folder.listFiles();
+        }
+        return new File[0];
+    }
 
 }
