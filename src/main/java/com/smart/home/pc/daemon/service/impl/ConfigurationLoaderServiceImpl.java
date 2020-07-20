5182df732f017c8e3aadc46176ff485495d49d11
Ramzi <Ramzi@Ramzus>
2020-07-20 14:39:14 +0200

    Code improvement
    - Move code to file helper class
    - Remove duplicate file existence check

--- src/main/java/com/smart/home/pc/daemon/service/impl/ConfigurationLoaderServiceImpl.java
+++ src/main/java/com/smart/home/pc/daemon/service/impl/ConfigurationLoaderServiceImpl.java
@@ -1,17 +1,10 @@
 package com.smart.home.pc.daemon.service.impl;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.Reader;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Date;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.google.gson.Gson;
 import com.smart.home.pc.daemon.dto.Config;
 import com.smart.home.pc.daemon.service.ConfigurationLoaderService;
 import com.smart.home.pc.daemon.util.FileHelper;
@@ -19,8 +12,6 @@ import com.smart.home.pc.daemon.util.FileHelper;
 public class ConfigurationLoaderServiceImpl implements ConfigurationLoaderService {
 
     private static final Logger LOGGER = LogManager.getLogger(ConfigurationLoaderServiceImpl.class);
-
-    private Gson gson = new Gson();
 
     private Date configurationFileLastModifiedDate;
 
@@ -38,19 +29,18 @@ public class ConfigurationLoaderServiceImpl implements ConfigurationLoaderServic
 
     @Override
     public Config loadConfiguration(String configurationPath) {
-        File configFile = new File(configurationPath);
-
+        Date configurationPathLastModificationDate = fileHelper.getFileLastModificationDate(configurationPath);
         // Configuration is found and is a file.
-        if (configFile != null && configFile.isFile()) {
-            configurationFileLastModifiedDate = new Date(configFile.lastModified());
-            Config loadedConfiguration = getConfigurationFromPath(configurationPath);
+        if (configurationPathLastModificationDate != null) {
+            configurationFileLastModifiedDate = configurationPathLastModificationDate;
+            Config loadedConfiguration = fileHelper.getConfigurationFromPath(configurationPath);
             if (loadedConfiguration != null) {
                 currentUserConfiguration = loadedConfiguration;
                 return currentUserConfiguration;
             }
         } else {
             // Configuration file not found or null
-            LOGGER.error("The provided configuration path was not found");
+            LOGGER.error("The provided configuration path was not found" + configurationPath);
         }
         return null;
     }
@@ -64,36 +54,6 @@ public class ConfigurationLoaderServiceImpl implements ConfigurationLoaderServic
             }
         }
         return true;
-    }
-
-    /**
-     * Reads the configuration file as a Json and returns the corresponding object
-     * 
-     * @param configFilePath
-     * @return
-     */
-    private Config getConfigurationFromPath(String configurationPath) {
-        Path configFilePath = Paths.get(configurationPath);
-        Config userConfiguration = null;
-
-        Reader reader;
-        try {
-            reader = Files.newBufferedReader(configFilePath);
-
-            // convert JSON file to map
-            userConfiguration = gson.fromJson(reader, Config.class);
-            reader.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            LOGGER.error("IOException while reading configuration file please check file format: " + configFilePath);
-        }
-
-        if (userConfiguration == null) {
-            LOGGER.error("Configuration file is either empty or malformed");
-        }
-
-        return userConfiguration;
     }
 
     public Date getConfigurationLoadDate() {
--- src/main/java/com/smart/home/pc/daemon/util/FileHelper.java
+++ src/main/java/com/smart/home/pc/daemon/util/FileHelper.java
@@ -1,9 +1,19 @@
 package com.smart.home.pc.daemon.util;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Date;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.google.gson.Gson;
+import com.smart.home.pc.daemon.dto.Config;
 
 /**
  * Helper class to manage file actions
@@ -12,6 +22,10 @@ import org.apache.commons.lang3.StringUtils;
  *
  */
 public class FileHelper {
+    
+    private static final Logger LOGGER = LogManager.getLogger(FileHelper.class);
+    
+    private Gson gson = new Gson();
 
     /**
      * Get last modification date for the given file path
@@ -30,6 +44,36 @@ public class FileHelper {
         }
 
         return null;
+    }
+    
+    /**
+     * Reads the configuration file as a Json and returns the corresponding object
+     * 
+     * @param configFilePath
+     * @return
+     */
+    public Config getConfigurationFromPath(String configurationPath) {
+        Path configFilePath = Paths.get(configurationPath);
+        Config userConfiguration = null;
+
+        Reader reader;
+        try {
+            reader = Files.newBufferedReader(configFilePath);
+
+            // convert JSON file to map
+            userConfiguration = gson.fromJson(reader, Config.class);
+            reader.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            LOGGER.error("IOException while reading configuration file please check file format: " + configFilePath);
+        }
+
+        if (userConfiguration == null) {
+            LOGGER.error("Configuration file is either empty or malformed");
+        }
+
+        return userConfiguration;
     }
 
 }
